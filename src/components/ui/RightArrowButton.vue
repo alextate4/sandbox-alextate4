d3f915c1f57d874f9c1fc259b9e5abe06ba70ae9
achurch8 <achurch8@stumail.jccc.edu>
2020-05-05 15:30:20 -0500

    Button components

---
+++ src/components/ui/RightArrowButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button> <h2>Planet Name</h2> > </button>
+</template>
+
+<style scoped>
+button {
+    background-color: white;
+    border: none;
+    padding: 15px 30px;
+    text-align: center;
+    text-decoration: none;
+    font-weight: bold;
+}
+h2 {
+    color: lightgrey;
+}
+</style>
