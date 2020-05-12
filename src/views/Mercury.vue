e61d58f0d3ef4d23f5b0b9f18eca898c0fbb8811
Kevin Atherton <katherto@gmail.com>
2020-05-12 10:02:50 -0500

    added temporary planet views

---
+++ src/views/Mercury.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mercury">
+    <h1>Mercury</h1>
+    <h2>Basic Statistics</h2>
+    <ul>
+      <li>Scaled radius: 0.25 meters (9.8 inches)</li>
+      <li>Relative size to earth: slightly larger than the Moon</li>
+      <li>Relative volume to earth: 18 Mercurys can fit into Earth</li>
+      <li>Relative mass to earth: 5.5% of Earth</li>
+      <li>Light travel time: 3.2 minutes</li>
+      <li>What’s it made out of: iron and silicate</li>
+      <li>Number of moons: 0</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mercury'
+};
+</script>
+
+<style scoped></style>
