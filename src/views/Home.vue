512e5d08a5f19988baa9cdc2ab4ddb6602c44142
Kevin Atherton <katherto@gmail.com>
2020-05-12 10:02:21 -0500

    removed examples

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,13 @@
 <template>
   <div class="home">
-    <input v-model="num1" type="number" />
-    <input v-model="num2" type="number" />
-    = {{ result }}
-    <div>
-      <button @click="calculate">Click</button>
-      {{ resultClick }}
-    </div>
-    <AppButton @click="calculate">Button</AppButton>
-    <AppButton to="/about">Router Link</AppButton>
-    <AppButton href="/about">&lt;a&gt; Link</AppButton>
+    <h1>Kansas Map Page</h1>
   </div>
 </template>
 
 <script>
-import AppButton from '../components/ui/AppButton.vue';
-import multiplication from '../utils/math';
-
 export default {
-  name: 'Home',
-  components: {
-    AppButton
-  },
-  data() {
-    return {
-      num1: 0,
-      num2: 0,
-      resultClick: 0
-    };
-  },
-  computed: {
-    result() {
-      return multiplication(this.num1, this.num2);
-    }
-  },
-  methods: {
-    calculate() {
-      this.resultClick = multiplication(this.num1, this.num2);
-    }
-  }
+  name: 'Home'
 };
 </script>
+
+<style scoped></style>
