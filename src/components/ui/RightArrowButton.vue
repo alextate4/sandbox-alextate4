bd9efc6cf9d1dc4f81af807d9062bfee9aba862d
Kevin Atherton <katherto@gmail.com>
2020-05-12 15:50:11 -0500

    previous pr corrected by linting

--- src/components/ui/RightArrowButton.vue
+++ src/components/ui/RightArrowButton.vue
@@ -1,17 +1,20 @@
 <template>
-    <button> <h2>Planet Name</h2> > </button>
+  <button>
+    <h2>Planet Name</h2>
+  </button>
 </template>
 
 <style scoped>
 button {
-    background-color: white;
-    border: none;
-    padding: 15px 30px;
-    text-align: center;
-    text-decoration: none;
-    font-weight: bold;
+  background-color: white;
+  border: none;
+  padding: 15px 30px;
+  text-align: center;
+  text-decoration: none;
+  font-weight: bold;
 }
+
 h2 {
-    color: lightgrey;
+  color: lightgrey;
 }
 </style>
--- src/views/PlanetInfo.vue
+++ src/views/PlanetInfo.vue
@@ -1,49 +1,47 @@
 <template>
-    <div class="container">
-        <img class="planetimg" src="image-placeholder.jpg" alt="planet">
-        <h2>Jupiter</h2>
-        <p>Information</p>
-        <div class="buttongroup">
-            <button>More Info</button>
-            <button>Nearby POI</button>
-        </div>
+  <div class="container">
+    <img class="planetimg" src="image-placeholder.jpg" alt="planet" />
+    <h2>Jupiter</h2>
+    <p>Information</p>
+    <div class="buttongroup">
+      <button>More Info</button>
+      <button>Nearby POI</button>
     </div>
+  </div>
 </template>
-
-
 
 <style scoped>
 .container {
-    font-family: sans-serif;
-    color: white;
-    width: 768px;
-    background: #0A0E28;
+  font-family: sans-serif;
+  color: white;
+  width: 768px;
+  background: #0a0e28;
 }
 
 .planetimg {
-    width: 80%;
-    margin: 10%;
+  width: 80%;
+  margin: 10%;
 }
 
-h2{
-    text-align: center;
+h2 {
+  text-align: center;
 }
 
-p{
-    width: 80%;
-    margin: 1em auto;
+p {
+  width: 80%;
+  margin: 1em auto;
 }
 
 .buttongroup {
-    text-align: center;
+  text-align: center;
 }
 
-button{
-    margin: 1em 2em;
-    border: none;
-    background: white;
-    padding: 1em;
-    font-weight: bold;
-    border-radius: 50%;
+button {
+  margin: 1em 2em;
+  border: none;
+  background: white;
+  padding: 1em;
+  font-weight: bold;
+  border-radius: 50%;
 }
 </style>
