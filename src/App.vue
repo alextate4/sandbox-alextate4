1722eb03447a94eed46c443f6aa2ed96be4273c4
Kevin Atherton <katherto@gmail.com>
2020-05-17 11:03:40 -0500

    modified links for dynamic routing

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/mercury">Mercury</router-link> |
-      <router-link to="/earth">Earth</router-link> |
-      <router-link to="/jupiter">Jupiter</router-link>
+      <router-link to="/planet/mercury">Explore Planet</router-link>
     </div>
     <router-view />
   </div>
