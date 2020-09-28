b9eca1f9bc111ada79dfabf40cb2b0729966d0dd
Kevin Atherton <katherto@gmail.com>
2020-09-27 21:55:03 -0500

    refactor: utility class fix

--- pages/planet.vue
+++ pages/planet.vue
@@ -15,7 +15,7 @@
       />
       <div class="mt-8 flex justify-between items-center">
         <div
-          class="h-12 w-8 rounded-tr-full rounded-br-full relative flex items-center opacity-50 bg-slateGray text-bgIndigo"
+          class="h-12 w-8 rounded-r-full relative flex items-center opacity-50 bg-slateGray text-bgIndigo"
         >
           <span class="pl-2 absolute left-0 text-xl">&lt;</span>
         </div>
@@ -24,7 +24,7 @@
           <span class="text-xl text-mercuryLavender">The Swift Planet</span>
         </div>
         <div
-          class="h-12 w-8 rounded-tl-full rounded-bl-full relative flex items-center opacity-50 bg-slateGray text-bgIndigo"
+          class="h-12 w-8 rounded-l-full relative flex items-center opacity-50 bg-slateGray text-bgIndigo"
         >
           <span class="pr-2 absolute right-0 text-xl">&gt;</span>
         </div>
