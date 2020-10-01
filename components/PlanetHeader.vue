0b4f586164f56b5fe7430b8bf27da04befe7345c
Kevin Atherton <katherton@pyxl.com>
2020-10-01 13:19:38 -0500

    refactor: componentize progress

---
+++ components/PlanetHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="px-4 h-12 flex justify-between bg-bgIndigo">
+      <div class="flex items-center">
+        <div class="text-veryLightGray text-xl">&lt;</div>
+        <span class="ml-2 text-base text-veryLightGray">Back</span>
+      </div>
+      <div class="flex items-center text-base text-veryLightGray">Menu</div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/planet.vue
+++ pages/planet.vue
@@ -1,34 +1,13 @@
 <template>
   <div>
-    <nav class="px-4 h-12 flex justify-between bg-bgIndigo">
-      <div class="flex items-center">
-        <div class="text-veryLightGray text-xl">&lt;</div>
-        <span class="ml-2 text-base text-veryLightGray">Back</span>
-      </div>
-      <div class="flex items-center text-base text-veryLightGray">Menu</div>
-    </nav>
+    <PlanetHeader />
     <main class="bg-bgIndigo">
       <img
         class="h-64 w-64 mx-auto"
         src="../assets/planets/mercury.png"
         alt="An image of planet Mercury"
       />
-      <div class="mt-8 flex justify-between items-center">
-        <div
-          class="h-12 w-8 rounded-r-full relative flex items-center opacity-50 bg-slateGray text-bgIndigo"
-        >
-          <span class="pl-2 absolute left-0 text-xl">&lt;</span>
-        </div>
-        <div class="text-center">
-          <h1 class="text-4xl text-veryLightGray">Mercury</h1>
-          <span class="text-xl text-mercuryLavender">The Swift Planet</span>
-        </div>
-        <div
-          class="h-12 w-8 rounded-l-full relative flex items-center opacity-50 bg-slateGray text-bgIndigo"
-        >
-          <span class="pr-2 absolute right-0 text-xl">&gt;</span>
-        </div>
-      </div>
+      <PlanetNav />
     </main>
   </div>
 </template>
